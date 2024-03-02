c9648db5481b574d22e7e9a983dfaa41a8e42a92
“Tamim239” <mdibrahimislamtamim239@gmail.com>
2024-03-02 12:39:04 +0600

    create api and setup website simple

---
+++ js/main.js
@@ -0,0 +1,409 @@
+const rokmariApi = [
+    {
+        "bookTitle": "রমাদান প্ল্যানার (সংস্করণ, ২০২৪)",
+        "bookAuthor": "শায়খ আহমাদুল্লাহ",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Ramadan_Planner_Edition_2024-Shaykh_Ahmadullah-3d107-380632.jpg"
+    },
+    {
+        "bookTitle": "আমি আবু বকর",
+        "bookAuthor": "আসিফ নজরুল ",
+        "bookPrice": "180 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Ami_Abu_Bakar-Asif_Nazrul-2d942-371268.jpg"
+    },
+    {
+        "bookTitle": "আগুনডানা মেয়ে",
+        "bookAuthor": "সাদাত হোসাইন ",
+        "bookPrice": "220 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Agundana_Meye-Sadat_Hossain_-6642c-360030.jpg"
+    },
+    {
+        "bookTitle": "মা, মা, মা এবং বাবা - দ্বিতীয় খণ্ড",
+        "bookAuthor": "সমকালীন সংকলন টিম",
+        "bookPrice": "90 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Ma_Ma_Ma_and_Baba_-Somokalin_Editor_Team-b6320-363165.jpg"
+    },
+    {
+        "bookTitle": "স্মার্ট মার্কেটিং ",
+        "bookAuthor": "মার্ক অনুপম মল্লিক",
+        "bookPrice": "230 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Smart_Marketing-Mark_Anupam_Mallick-e7693-366556.jpg"
+    },
+    {
+        "bookTitle": "কোটিপতি সাহাবি",
+        "bookAuthor": "আরিফুল ইসলাম",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Kotipoti_Sahabi-Ariful_Islam-058ed-316419.jpg"
+    },
+    {
+        "bookTitle": "তোমার জন্য দাঁড়িয়ে ছিলাম বলে",
+        "bookAuthor": "সাদাত হোসাইন ",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Tomar_Jonno_Dariye_Chilam_Bole-Sadat_Hossain_-2465e-368730.jpg"
+    },
+    {
+        "bookTitle": "সন্তান প্রতিপালনে এ যুগের চ্যালেঞ্জ ",
+        "bookAuthor": "ড. মোহাম্মদ সরোয়ার হোসেন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Sontan_Protipalone_e_juger_challange-Dr_Muhammad_Sarwar_Hossain-6b9c0-332441.jpg"
+    },
+    {
+        "bookTitle": "দ্বীনে ফেরার পর হারিয়ে যেয়ো না",
+        "bookAuthor": "ডা. শামসুল আরেফীন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Dine_Ferar_Por_Hariye_Jeo_Na-Dr_Shamsul_Arafin-ad6c9-344255.png"
+    },
+    {
+        "bookTitle": "চ্যাট জিপিটি অ্যান্ড এ আই মিলিয়নেয়ার",
+        "bookAuthor": "মেহেদী হাসান রিফাত",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Chat_GPT_and_AI_Millionaire-Mehdi_Hasan_Rifat-21911-344471.png"
+    },
+    {
+        "bookTitle": "দৈনন্দিন মাসনূন দুআ ও যিকর ",
+        "bookAuthor": "ড. খোন্দকার আব্দুল্লাহ জাহাঙ্গীর",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Doinondin_Jibone_Duya_o_Zikir-Dr_Khandaker_Abdullah_Jahangir-527fd-334476.jpg"
+    },
+    {
+        "bookTitle": "পড়ো ৪ ",
+        "bookAuthor": "ওমর আল জাবির",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Poro_4-Omor_Al_Jabir-48c51-319569.png"
+    },
+    {
+        "bookTitle": "কসমোজাহি",
+        "bookAuthor": "মোহাম্মদ নাজিম উদ্দিন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Kosmojahi-Mohammod_Nazim_Uddin-d70e0-344127.jpg"
+    },
+    {
+        "bookTitle": "প্রবেশ নিষেধ",
+        "bookAuthor": "কয়েস সামী",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Probesh_Nished-Koyes_Sami-b8e39-341717.jpeg"
+    },
+    {
+        "bookTitle": "নাইয়রি",
+        "bookAuthor": "মৌরি মরিয়ম",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Nayoree-Mouri_Morium-5ff26-363869.jpg"
+    },
+    {
+        "bookTitle": "তিনিই আমার রব - ৩য় খণ্ড",
+        "bookAuthor": "শাইখ ড. রাতিব আন-নাবুলুসি",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Tinie_Amar_Rob_3-Shaikh_Dr_Ratib_An_Nabulusi-43800-319568.png"
+    },
+    {
+        "bookTitle": "পর্দা গাইডলাইন ",
+        "bookAuthor": "তানজিল আরেফীন আদনান",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Porda_guideline_-Tanjil_Arefin_Adnan-498ae-344578.jpg"
+    },
+    {
+        "bookTitle": "মৃত্যু যখন উপহার ",
+        "bookAuthor": "মোঃ মতিউর রহমান",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Mrittu_Jokhon_Upohar-Md_Matiur_Rahman-39da3-333589.jpg"
+    },
+    {
+        "bookTitle": "দি আর্ট অব পারসোনাল ফাইন্যান্স ম্যানেজমেন্ট ",
+        "bookAuthor": "সাইফুল হোসেন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/The_Art_Of_Personal_Finance_Management-Saiful_Hossain-8914e-369789.jpg"
+    },
+    {
+        "bookTitle": "কথা বলার কৌশল",
+        "bookAuthor": "ল্যারি কিং",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/How_to_Talk_to_Anyone-Larry_King-3ddd3-338046.png"
+    },
+    {
+        "bookTitle": "সবিনয় নিবেদন এই যে... ",
+        "bookAuthor": "আশীব ফেরদৌস অংকন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Sobinoi_Nibedon_Ei_Je-Ashib_Ferdous_Ankon-31d97-356626.jpg"
+    },
+    {
+        "bookTitle": "অপেক্ষায় বিরামচিহ্ন নেই",
+        "bookAuthor": "সুমাইয়া করিম",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Opekkhay_Biramchinho_Nei_-Sumaiya_Korim-5592a-365996.jpg"
+    },
+    {
+        "bookTitle": "আর রাহিকুল মাখতুম",
+        "bookAuthor": "আল্লামা সফিউর রহমান মুবারকপুরী (রহ.)",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Ar_Rahikul_Makhtum-Allama_Sofiur_Rahman_Mubarakpuri_Rh-94947-363158.png"
+    },
+    {
+        "bookTitle": "বেসিক আলী ১৬ ",
+        "bookAuthor": "শাহরিয়ার",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Basic_Ali_16-Shahorier-d2422-363791.png"
+    },
+    {
+        "bookTitle": "রি-স্টার্ট ইয়োর লাইফ",
+        "bookAuthor": "মুহাম্মদ ইলিয়াস কাঞ্চন (কোচ কাঞ্চন)",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Re_Start_Your_Life-Muhammod_Eleyas_kanchon_Coach_Kanchan-5a673-315922.jpg"
+    },
+    {
+        "bookTitle": "কিয়ামতের আলামত ও শেষ যুগের ভয়াবহ ফিতনা ",
+        "bookAuthor": "মীযান হারুন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Kiyamoter_Alamot_O_Shesh_Juger_Bhoyaboho-Mijan_Harun-cef44-346475.jpg"
+    },
+    {
+        "bookTitle": "তিনিই আমার রব - চতুর্থ খণ্ড",
+        "bookAuthor": "শাইখ আলী জাবির আল-ফাইফি",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Tini_Amar_Rob_-Shaikh_Ali_Jabir_Al_Fayfe-7b85b-363427.jpg"
+    },
+    {
+        "bookTitle": "কনটেন্ট রাইটিং ইজ কিং",
+        "bookAuthor": "অ্যান হ্যান্ডলি",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/___-Ann_Handley-91dea-332093.jpg"
+    },
+    {
+        "bookTitle": "নির্ঝর ও একটি হলুদ গোলাপ",
+        "bookAuthor": "নিশো আল মামুন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Nirjhor_o_Ekti_Holud_Golap-Nisho_Al_Mamun-4c7c3-368118.jpg"
+    },
+    {
+        "bookTitle": "গুনাহ মাফের উপায়",
+        "bookAuthor": "শাহাদাৎ হুসাইন খান ফয়সাল",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Gunah_Maper_Upaiy-Shahadat_Hossain_Khan_Faysal-56282-363423.jpg"
+    },
+    {
+        "bookTitle": "চুয়াত্তরের দুর্ভিক্ষ ",
+        "bookAuthor": "মহিউদ্দিন আহমদ",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Chohaturrer_Durvikkho-Mohiuddin_Ahmmod-61ad3-364734.jpg"
+    },
+    {
+        "bookTitle": "শরতের শেষ থেকে",
+        "bookAuthor": "ফারহানা সিনথিয়া",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Shoroter_Shesh_Theke-Farhana_Sinthiya-e763b-365690.jpg"
+    },
+    {
+        "bookTitle": "তেহাত্তরের নির্বাচন ",
+        "bookAuthor": "মহিউদ্দিন আহমদ",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Tehaturrer_Nirbachon-Mohiuddin_Ahmmod-483da-364736.jpg"
+    },
+    {
+        "bookTitle": "বাছাইকৃত অতি প্রয়োজনীয় Vocabulary ",
+        "bookAuthor": "আব্দুল আউয়াল",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/_Selected_Essential_Vocabulary-Abdul_Awal-3a760-331982.jpg"
+    },
+    {
+        "bookTitle": "জিনজাতির বিস্ময়কর জগৎ",
+        "bookAuthor": "আবু আহমাদ সারোয়ার",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Jinjatir_Bismoykor_Jogod-Abu_Ahmad_Sarwar-ca2ea-363426.png"
+    },
+    {
+        "bookTitle": "গল্প নয়, একমুঠো আলো",
+        "bookAuthor": "আদহাম শারকাভি",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Golpo_Noi_Akmotho_Alo-Adham_Sharkavi-afbc8-363422.jpg"
+    },
+    {
+        "bookTitle": "ফেরেশতাদের জগৎ",
+        "bookAuthor": "শাইখ ড. উমার সুলাইমান আশকার",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Fereshtader_Jogot-_Sheikh_Dr_Umar_Sulaiman_Ashkar-fce03-363163.png"
+    },
+    {
+        "bookTitle": "আদাবুল ইখতিলাফ ",
+        "bookAuthor": "শাইখ মুহাম্মাদ আওয়ামা ",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Adabul_Ikhtilaf-_Shaykh_Muhammad_Awama-04a93-346106.jpg"
+    },
+    {
+        "bookTitle": "আত্মশুদ্ধির ব্যাবহারিক পাঠ ",
+        "bookAuthor": "উস্তায মাহমুদ তাওফিক",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Attosuddhir_Baboharik_Path-Ustaz_Mahmud_Tawfiq-18731-346472.jpg"
+    },
+    {
+        "bookTitle": "যে দুপুর বাহানা বানায় ",
+        "bookAuthor": "স্বপ্নীল চক্রবর্ত্তী",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Je_Dupur_Bahana_Banai-Swapnil_Chakrabarty-51632-359567.jpg"
+    },
+    {
+        "bookTitle": "মুসা আলাইহিস সালামের ঘটনাবহুল জীবন এবং আমাদের জন্য রেখে যাওয়া শিক্ষা",
+        "bookAuthor": "শাইখ আলী জাবির আল-ফাইফি",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Musa_Alaihis_Salamer_Gotonabhul_Jibon_an-Shaikh_Ali_Jabir_Al_Fayfe-3148a-363167.png"
+    },
+    {
+        "bookTitle": "কিতাবুল বুয়ু",
+        "bookAuthor": "মুফতী আব্দুল্লাহ মাসুম",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Kitabul_Bayu-Mufti_Abdullah_Masum-cd673-363421.jpg"
+    },
+    {
+        "bookTitle": "হিজলের ফুল জলে ভাসা ভুল",
+        "bookAuthor": "শানজানা আলম",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Hijoler_Ful_Jole_Vasa_Vul-Shanjana_Alom-6ce51-364846.jpg"
+    },
+    {
+        "bookTitle": "সিরাতে ইবনে হিশাম",
+        "bookAuthor": "আবূ মুহাম্মদ আবদুল মালিক ইব্‌ন হিশাম মুআফিরী (র)",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Sirate_Ibne_Hisham-Abu_Muhammad_Abdul_Malik_Ibn_Hisham_Muaf-16cf4-363420.jpg"
+    },
+    {
+        "bookTitle": "অহীর আয়নায় পরকাল ",
+        "bookAuthor": "ড. আবুল কালাম আজাদ (বাশার)",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Wahir_aynai_porokal-Dr_Abul_Kalam_Azad_Bashar-31e67-350282.png"
+    },
+    {
+        "bookTitle": "ভ্রান্তির সমাধি ",
+        "bookAuthor": "মুহাম্মাদ মুশফিকুর রহমান মিনার",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Vrantir_Somadhi-Muhammad_Mushofikur_Rahman_Minar-bff3c-346706.jpg"
+    },
+    {
+        "bookTitle": "দার্জিলিংয়ের টুপি",
+        "bookPrice": "120 taka",
+        "bookAuthor": "তৃধা আনিকা",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Darjeelinger_Tupi-Tridha_Anika-9afda-361524.jpg"
+    },
+    {
+        "bookTitle": "কুরআন সংকলনের ইতিহাস",
+        "bookAuthor": "ডঃ মুহাম্মাদ মুস্তাফা আল আযামী",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Quran_Sonkoloner_Itihas-Dr_Muhammad_Mustafa_Al_Azami-f5946-363171.jpg"
+    },
+    {
+        "bookTitle": "ফতোয়া লেখার কলাকৌশল",
+        "bookPrice": "120 taka",
+        "bookAuthor": "মুফতী আব্দুল্লাহ মাসুম",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Fotoyar_Lekhar_Kowshol-Shaikh_Ibrahim_As_Sakran_-fd1f4-363174.jpg"
+    },
+    {
+        "bookTitle": "স্বপ্নকে সময় দিন ",
+        "bookAuthor": "জাবিদুল ইসলাম",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Shawpnoke_Somoy_Din-Jabidul_Islam-3ccda-364947.jpg"
+    },
+    {
+        "bookTitle": "পরানবন্দী",
+        "bookAuthor": "ডা. শামসুল আরেফীন",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Poranbondi-Dr_Shamsul_Arafin-66a75-340165.png"
+    },
+    {
+        "bookTitle": "ইসলামি শিক্ষাব্যবস্থার ইতিহাস ",
+        "bookPrice": "120 taka",
+        "bookAuthor": "ড. আহমাদ শালাবী",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/History_of_Islamic_Education-Dr_Ahmad_Shalaby-71a6c-340155.png"
+    },
+    {
+        "bookTitle": "ইসলামি অর্থব্যবস্থার ইতিহাস ",
+        "bookPrice": "120 taka",
+        "bookAuthor": "আহমেদ আল আশকার",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/History_of_Islamic_Economics_-Ahmad_Al_Ashkar-58abb-340167.png"
+    },
+    {
+        "bookTitle": "মৃত্যুবাড়ি ",
+        "bookPrice": "120 taka",
+        "bookAuthor": "মোশতাক আহমেদ",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Mrittubari-Mostaque_Ahamed-baaf2-375358.jpg"
+    },
+    {
+        "bookTitle": "জিলহজ্জের প্রথম দশক ",
+        "bookAuthor": "শাইখ মুহাম্মাদ সালেহ আল মুনাজ্জিদ",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Zilhazer_Prothom_Doshok-Shaikh_Mohammad_Saleh_Al_Munajjid-37422-312554.png"
+    },
+    {
+        "bookTitle": "অসংগতি ",
+        "bookPrice": "120 taka",
+        "bookAuthor": "আবদুল্লাহ আল মাসউদ",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Osonggoti-Abdullah_Al_Masud-247f0-306082.png"
+    },
+    {
+        "bookTitle": "আল আযকার ",
+        "bookPrice": "120 taka",
+        "bookAuthor": "ইমাম মুহিউদ্দীন ইয়াহইয়া আন-নববী (রহ.)",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Al_Azkar-Imam_an_Nobobi_Rh-7a7a3-332195.jpg"
+    },
+    {
+        "bookTitle": "Fun Crossword Puzzles For Kids",
+        "bookAuthor": "তাবাস্সুম মোস্লেহ বুশরা",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Fun_Crossword_Puzzles_For_Kids-Tabassum_Mosleh_Bushra-28631-355008.jpg"
+    },
+    {
+        "bookTitle": "Children's Dua and Hadis Package - English Version",
+        "bookAuthor": "সন্দীপন টীম",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Childrens_Dua_and_Hadis_Package_-Sandeepan_team-0f290-363348.png"
+    },
+    {
+        "bookTitle": "দরুদ ও সালাম ",
+        "bookAuthor": "মাওলানা মাহবুবুল হাসান আরিফী",
+        "bookPrice": "120 taka",
+        "bookImg": "https://ds.rokomari.store/rokomari110/ProductNew20190903/130X186/Dorud_and_Salam-Mawlana_Mahbubul_Hasan_Arifi-b69d0-374459.png"
+    }
+]
+
+
+
+const handleApi = (data, isShowAll) => {
+    const bookContainer = document.getElementById("book-container");
+    const showALLBtn = document.getElementById("show-all-btn")
+    bookContainer.textContent = '';
+    console.log(data.length)
+    if (data.length > 6 && !isShowAll) {
+        showALLBtn.classList.remove("hidden");
+    }
+    else {
+        showALLBtn.classList.add("hidden");
+    }
+
+    if (!isShowAll) {
+        data = data.slice(0, 6);
+    }
+    console.log(isShowAll)
+
+    data.forEach(book => {
+        // console.log(book)
+        const div = document.createElement("div");
+        div.innerHTML = `
+    <div class="card h-80 card-compact shadow-xl">
+    <figure><img src="${book.bookImg}" alt="Shoes" /></figure>
+      <div class="card-body">
+        <h2 class="card-title">${book.bookTitle}</h2>
+        <p class="text-xl">${book.bookAuthor}</p>
+        <p class="text-xl font-bold">${book.bookPrice}</p>
+        <div class="card-actions justify-end">
+          <button class="btn btn-secondary">Buy Now</button>
+        </div>
+      </div>
+    </div>
+    `
+        bookContainer.appendChild(div)
+    });
+}
+// show all btn
+const handleShowALlBtn = (isShowAll) => {
+
+    handleApi(rokmariApi,isShowAll)
+}
+
+handleApi(rokmariApi)
+
